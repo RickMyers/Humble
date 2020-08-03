e880fd0c7c2c6e378a5dd9cfaabdbbc3fa6d3eb9
Rick Myers <rickmyers1969@gmail.com>
2020-08-03 12:45:07 -0700

    Incremental

--- app/Code/Base/Humble/web/js/ckeditor/config.js
+++ app/Code/Base/Humble/web/js/ckeditor/config.js
@@ -7,4 +7,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+        config.resize_enabled = false;
+        config.entities_latin = false;
+        config.entities_greek = false; 
+        config.entities       = false; 
+        config.basicEntities  = false;            
 };
