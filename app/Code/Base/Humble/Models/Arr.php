c1a60793f73426eeedab8d5b27416d90c3d9aae8
Rick Myers <rickmyers1969@gmail.com>
2021-03-06 17:08:34 -0800

    Ergh...

--- app/Code/Base/Humble/Models/Arr.php
+++ app/Code/Base/Humble/Models/Arr.php
@@ -113,7 +113,7 @@ class Arr extends \ArrayObject
      */
     public function __call($func, $argv) {
         if (!is_callable($func) || substr($func, 0, 6) !== 'array_') {
-            throw new BadMethodCallException(__CLASS__.'->'.$func);
+            throw new \BadMethodCallException(__CLASS__.'->'.$func);
         }
         return call_user_func_array($func, array_merge(array($this->getArrayCopy()), $argv));
     }    
