57a4b24fa27d510b1268918b2222a75c0ec397d3
rickmyers1969 <rmyers@argusdentalvision.com>
2022-05-28 15:12:54 -0700

    Fix for install schema corruption

--- app/Code/Base/Humble/Schema/Install/install.sql
+++ app/Code/Base/Humble/Schema/Install/install.sql
@@ -12,10 +12,6 @@ MySQL - 8.0.23 : Database - humble
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`humble` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-USE `humble`;
-
 /*Table structure for table `humble_categories` */
 
 CREATE TABLE `humble_categories` (
