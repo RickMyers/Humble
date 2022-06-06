d3063678491ba6f73948efea6c6472663f01f521
rickmyers1969 <rmyers@argusdentalvision.com>
2022-06-06 11:19:54 -0700

    Import/Install/Export work

---
+++ app/FileManager.php
@@ -0,0 +1,22 @@
+<?php
+/**
+                   _______ __               
+                  / ____(_) /__             
+                 / /_  / / / _ \            
+    __  ___     / __/ / / /  __/            
+   /  |/  /___ /_/__ /_/_/\___/_ ____  _____
+  / /|_/ / __ `/ __ \/ __ `/ __ `/ _ \/ ___/
+ / /  / / /_/ / / / / /_/ / /_/ /  __/ /    
+/_/  /_/\__,_/_/ /_/\__,_/\__, /\___/_/     
+                         /____/             
+  Just drives the periodic check for files
+          arriving in folders
+ */
+
+require "Humble.php";
+require "Constants.php";
+ob_start();
+$spooler = Humble::getModel('paradigm/system');
+$spooler->manageFiles();
+
+//END
