e469fa4dad39608f0d428fc34c28c1e7451c3325
Rick Myers <rickmyers1969@gmail.com>
2022-07-03 14:36:18 -0700

    Some admin stuff getting moved around and began work on the Contrive Designer for controllers

---
+++ app/Code/Base/Contrive/Helpers/Helper.php
@@ -0,0 +1,21 @@
+<?php
+namespace Code\Base\Contrive\Helpers;
+/**
+ *
+ * Utility Base Transfer Class
+ *
+ * All helper classes for this module must extend this class
+ *
+ * PHP version 7.0+
+ *
+ * LICENSE:
+ *
+ * @category   Utility
+ * @package    Contrive
+ * @author     Original Author <rick@humbleprogramming.com>
+ */
+class Helper extends \Code\Base\Humble\Helpers\Helper
+{
+
+}
+?>
---
+++ app/Code/Base/Contrive/web/js/Contrive.js
@@ -0,0 +1,11 @@
+Humble.contrive = (function () {
+    //local variables and functions here
+    return {
+        foo: function () {
+
+        },
+        bar: function () {
+
+        }
+    }
+})();
