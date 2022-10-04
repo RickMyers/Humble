a364a7731a9a7bbc7ab886f406f7f0ddefb71106
rickmyers1969 <rmyers@argusdentalvision.com>
2022-10-04 12:29:19 -0700

    Incrementing version

--- app/Code/Base/Humble/Models/Documentation.php
+++ app/Code/Base/Humble/Models/Documentation.php
@@ -21,8 +21,11 @@ class Documentation extends Model
 
     use \Code\Base\Humble\Event\Handler;
     
+    //--------------------------------------------------------------------------
+    //We will just default to PHP Documentor just for S&Gs
     private string $documentor          = 'PHPDoc2.phar';
     private string $documentor_source   = 'https://phpdoc.org/phpDocumentor.phar';
+    private string $command             = 'php PHPDoc2.phar';
 	
     /**
      * Constructor
@@ -41,12 +44,17 @@ class Documentation extends Model
     }
 
     /**
-     * Checks to see if the default documentor is already downloaded, and if not, then we go get it
+     * Checks to see if the default documentor (or other) is already downloaded, and if not, then we go get it
      * 
      * @TODO: Allow at the system level to choose which documentation tool you wish to use
      * @return boolean
      */
     protected function documentorExists() {
+        if ($documentor = Environment::getApplication('documentation')) {
+            $this->documentor        = isset($documentor['engine'])  ? $documentor['engine']  : false;
+            $this->documentor_source = isset($documentor['source'])  ? $documentor['source']  : false;
+            $this->command           = isset($documentor['command']) ? $documentor['command'] : false;
+        }
         if (!$exists = file_exists($this->documentor)) {
             $exists = file_put_contents($this->documentor,file_get_contents($this->documentor_source));
         }
@@ -64,13 +72,13 @@ class Documentation extends Model
         if (Environment::getApplication('state') !== 'PRODUCTION') {
             chdir('..');
             if ($this->documentorExists()) {
-                $results = shell_exec(PHP_BINARY.' PHPDoc2.phar 2>&1');
+                $results = shell_exec(PHP_BINARY.$this->command.' 2>&1');
                 if ($EVENT) {
                     $EVENT->update(['documentation_generation_results'=>$results]);
                 }
                 //$this->trigger('APIDocumentationGenerated',__CLASS__,__METHOD__,['generated'=>date('Y-m-d H:i:s'),'generator'=>Environment::whoAmIReally()]);
             } else {
-                $results = 'Error doing stuff';
+                $results = 'Could not resolve the documentation engine to use';
             }
             chdir('app');
         }
