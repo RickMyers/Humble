69178f561fc84f731cc8769a4ac0a05750b87c8e
rickmyers1969 <rick@humbleprogramming.com>
2024-06-08 14:37:36 -0700

    I put the cart before the horse...

--- app/install/Humble.php
+++ app/install/Humble.php
@@ -308,10 +308,7 @@ FACTORY;
         $repl[] = $project->factory_name.'.php';
     }
     //file_put_contents('index.php',str_replace($srch,$repl,file_get_contents('index.php')));  //replacing default Humble factory with the custom one you just created... bad idea maybe?
-    $srch = ['{$name}','{$version}','{$serial_number}','{$enabled}','{$polling}','{$interval}','{$installer}','{$quiescing}','{$SSO}','{$authorized}','{$idp}','{$caching}'];
-    $repl = [$project->project_name,$remote->version,$project->serial_number,'1','0','15','1','0','0','0','','1'];
-    @mkdir('app/Code/'.$project->package.'/'.$project->module.'/etc/',0775,true);
-    file_put_contents('app/Code/'.$project->package.'/'.$project->module.'/etc/application.xml',str_replace($srch,$repl,file_get_contents('app/Code/Framework/Humble/lib/sample/install/etc/application.xml')));
+
     print("\n\n");
     
     chdir('app');
@@ -333,6 +330,10 @@ FACTORY;
     foreach ($output as $result) {
         print($result."\n");
     }
+    $srch = ['{$name}','{$version}','{$serial_number}','{$enabled}','{$polling}','{$interval}','{$installer}','{$quiescing}','{$SSO}','{$authorized}','{$idp}','{$caching}'];
+    $repl = [$project->project_name,$remote->version,$project->serial_number,'1','0','15','1','0','0','0','','1'];
+    @mkdir('app/Code/'.$project->package.'/'.$project->module.'/etc/',0775,true);
+    file_put_contents('app/Code/'.$project->package.'/'.$project->module.'/etc/application.xml',str_replace($srch,$repl,file_get_contents('app/Code/Framework/Humble/lib/sample/install/etc/application.xml')));    
     chdir('..');
     $x = (file_exists('humble.bat')) ? @unlink('humble.bat') : '';
     $x = (file_exists('humble.sh'))  ? @unlink('humble.sh') : '';
