75d91587a029705c17e8258994115a556bc01bbe
rickmyers1969 <rick@humbleprogramming.com>
2024-11-26 07:51:24 -0800

    Adding a test app

---
+++ app/Code/Framework/Humble/lib/sample/install/includes/test.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  ______          __            
+ /_  __/__  _____/ /_           
+  / / / _ \/ ___/ __/           
+ / / /  __(__  ) /_             
+/_/ _\___/____/\__/             
+   / __ \_____(_)   _____  _____
+  / / / / ___/ / | / / _ \/ ___/
+ / /_/ / /  / /| |/ /  __/ /    
+/_____/_/  /_/ |___/\___/_/     
+                                
+Just a program for you to use for some dirty command line testing
+
+ */
+require "&&FACTORY&&.php"; 
+require "Environment.php";
+require "Code/Framework/Humble/includes/Constants.php";
+require "Code/&&PACKAGEG&&/&&MODULE&&/includes/Constants.php";
