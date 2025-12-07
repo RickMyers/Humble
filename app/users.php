b0224c3d73c15492804cb81ace4d40c96af22221
DESKTOP-129UVFO\Myers <rick@humbleprogramming.com>
2025-12-07 09:08:44 -0800

    Util

--- app/users.php
+++ app/users.php
@@ -1,11 +1,11 @@
 <?php
 
 require "Humble.php";
-/*
+
 $faker = Humble::fake();
 
 $user = Humble::entity('users');
-
+$password = '';
 
 for ($i=0; $i<200; $i++) {
     $f_name = $faker->firstName();
@@ -15,7 +15,6 @@ for ($i=0; $i<200; $i++) {
     $gender = $faker->gender();
     $dob    = $faker->date('01/01/1940','12/31/2016');
     print($f_name.' '.$l_name.'-'.$u_name.'/'.$email.'---'.$gender.'---'.$dob."\n");
-    $user->setFirstName($f_name)->setLastName($l_name)->setUserName($u_name)->setEmail($email)->setGender($gender)->setDateOfBirth($dob)->setPassword('jacob001')->newUser();
+    $user->setFirstName($f_name)->setLastName($l_name)->setUserName($u_name)->setEmail($email)->setGender($gender)->setDateOfBirth($dob)->setPassword($password)->newUser();
 }
- * 
- */
+ 
