515bcf9d5a10ecfccee03a9de207a50c79cb93ba
DESKTOP-129UVFO\Myers <rick@humbleprogramming.com>
2025-12-07 09:11:59 -0800

    Util fix

--- app/users.php
+++ app/users.php
@@ -15,6 +15,6 @@ for ($i=0; $i<200; $i++) {
     $gender = $faker->gender();
     $dob    = $faker->date('01/01/1940','12/31/2016');
     print($f_name.' '.$l_name.'-'.$u_name.'/'.$email.'---'.$gender.'---'.$dob."\n");
-    $user->setFirstName($f_name)->setLastName($l_name)->setUserName($u_name)->setEmail($email)->setGender($gender)->setDateOfBirth($dob)->setPassword($password)->newUser();
+    $user->reset()->setFirstName($f_name)->setLastName($l_name)->setUserName($u_name)->setEmail($email)->setGender($gender)->setDateOfBirth($dob)->setPassword($password)->newUser();
 }
  
