528cd94dca1775d12e1f26814ed04643a735a271
DESKTOP-129UVFO\Myers <rick@humbleprogramming.com>
2026-04-17 13:15:11 -0700

    Installation fixes

--- debug.php
+++ debug.php
@@ -2,7 +2,7 @@
 chdir('app');
 require "Humble.php";
 $state = strtoupper(Environment::application('state'));
-if (($state == 'DEVELOPMENT') || ($state == 'DEBUG') || ($state === 'TEST')) {
+if ((!$state) || ($state == 'DEVELOPMENT') || ($state == 'DEBUG') || ($state === 'TEST')) {
     phpinfo();    
 } else {
     print('Started...');
