7cca439ed5daf071ac5a04c33643f154dbf8b0c5
DESKTOP-C9MCKDQ\Rick <rick@humbleprogramming.com>
2026-07-02 15:28:55 -0700

    More tweaks to a program to create a large data file

--- app/pat_file.php
+++ app/pat_file.php
@@ -6,8 +6,10 @@ require "Environment.php";
 require "Code/Framework/Humble/includes/Constants.php";
 require "Code/Framework/Humble/includes/Custom.php";
 
+$action     = ['A','A','A','N','N','N','N','N','N','C','C','D','D'];
+$actions    = count($action);
 $faker      = Humble::fake();
-$pats       = 10000;
+$pats       = 1000000;
 $bulk       = 500;
 $tot        = 0;
 $ctr        = 0;
@@ -21,13 +23,13 @@ for ($i=0; $i<$pats; $i++) {
         "gender" => $faker->gender(),
         "email" => $faker->email(true),
         "date_of_birth" => $faker->date('01/01/1940','12/31/2016'),
-        "address" => $faker->streetAddress(),
+        "address" => $faker->number(4,100,4000).' '.$faker->streetAddress(),
         "city"   => $faker->city(),
         "state" => $faker->state(),
         "zip_code" => $faker->zipCode(),
         "phone_number" => $faker->phoneNumber(true)
     ];
-    $patients[] = json_encode($patient);
+    $patients[] = $action[rand(0,$actions-1)].'='.json_encode($patient);
     if ($ctr++ >= $bulk) {
         file_put_contents('patients.dat',implode("\n",$patients),FILE_APPEND);
         $ctr        = 0;
