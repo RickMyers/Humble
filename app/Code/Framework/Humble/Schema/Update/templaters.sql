81ca04e3bf3b23126490efd5017bb07b94cd53e4
DESKTOP-C9MCKDQ\Rick <rick@humbleprogramming.com>
2026-07-23 05:08:02 -0700

    Updating available templaters

--- app/Code/Framework/Humble/Schema/Update/templaters.sql
+++ app/Code/Framework/Humble/Schema/Update/templaters.sql
@@ -1,13 +1,15 @@
+truncate table `humble_templaters`;
+
 insert  into `humble_templaters`(`id`,`templater`,`extension`,`description`) values 
-(2,'Smarty','tpl','Smarty'),
-(3,'Twig','twig','Twig'),
-(4,'Blade','blade','Blade'),
-(5,'Rain3','rain','Rain 3'),
-(6,'TBS','tbs','Tiny But Strong'),
-(7,'PHP','php','PHP'),
-(8,'PHPTal','phptal','PHP TAL'),
-(9,'DWOO','phptal','DWOO (do not use)',
-(10,'Mustache','mustache','Mustache'),
-(11,'Latte','latte','Latte'),
-(12,'Savant','savant','Savant');
-(13,'Volt','phtml','Savant');
+(1,'Smarty','tpl','Smarty'),
+(2,'Twig','twig','Twig'),
+(3,'Blade','blade','Blade'),
+(4,'Rain3','rain','Rain 3'),
+(5,'TBS','tbs','Tiny But Strong'),
+(6,'PHP','php','PHP'),
+(7,'PHPTal','phptal','PHP TAL'),
+(8,'DWOO','tpl','DWOO (deprecated?)',
+(9,'Mustache','mustache','Mustache'),
+(10,'Latte','latte','Latte'),
+(11,'Savant','savant','Savant'),
+(12,'Volt','phtml','Volt');
